adbea53615865d7bc3814429d0119989212418a6
mattcaswell <matthewcaswell253@Gmail.com>
2017-05-05 14:37:01 -0700

    I Hate AJAX

--- src/MrFixIt/wwwroot/js/scripts.js
+++ src/MrFixIt/wwwroot/js/scripts.js
@@ -1,1 +1,13 @@
-﻿var test = "it works";
+﻿$(document).ready(function () {
+
+    $('.claims').click(function () {
+        $.ajax({
+            type: 'GET',
+            dataType: 'html',
+            url: '@Url.Action("request-url")',
+            success: function (result) {
+                $('#result').html(result);
+            }
+        });
+    });
+});
